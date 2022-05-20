61ef63ea00211391d5fb1ef8ae8172df5833288b
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-05-20 17:48:58 +0300

    Youtube MP3 Dönüştürücü

---
+++ indirme.js
@@ -0,0 +1,14 @@
+const { exec } = require("child_process");
+function indir(url) {
+    exec("youtube-dl " + url, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
